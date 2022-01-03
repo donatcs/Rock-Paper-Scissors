938123b75933ee554b3f183375f978574b277def
donatcs <csonkadonaaat@gmail.com>
2022-01-03 22:58:35 +0100

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 
+let playerScore = 0
+let computerScore = 0
+
+
 const answers = ["rock", "paper", "scissor"];
 
 const rock = document.querySelector('#rock');
@@ -17,13 +21,22 @@ scissor.addEventListener("click", function() {
 });
 
 
+///
+
+
+
+///
+
+
+
+
 function computerPlay() {
   return answers[Math.floor(Math.random() * answers.length)];
 }
+let result = ""
 
-function playerPlay() {
-  return answers[Math.floor(Math.random() * answers.length)];
-}
+
+
 
 function playRound() {
   const computerSelection = computerPlay();
@@ -31,23 +44,40 @@ function playRound() {
   
 
 
-   
 
   //PLAYER ROCK
-  if (playerSelection == "rock" && computerSelection == "paper")
-    console.log("You lost. Paper beats Rock!");
-  else if (playerSelection == "rock" && computerSelection == "rock")
-  console.log("You tied!");
-  else if (playerSelection == "rock" && computerSelection == "scissor")
-  console.log("You won. Rock beats scissors!");
+  if (playerSelection == "rock" && computerSelection == "paper") {
+    computerScore += 1
+    result = ('You lost!' + "<br>Computer Score:" + computerScore + "<br>Player Score:" + playerScore)
+    document.getElementById('result').innerHTML = result
+    
+  }
+  
+  else if (playerSelection == "rock" && computerSelection == "rock") {
+  computerScore += 0
+  playerScore += 0
+  result = ('Thats a tie!' + "<br>Player Score:" + playerScore + "<br>Computer Score:" + computerScore)
+  document.getElementById('result').innerHTML = result
+  
+}
+
+  else if (playerSelection == "rock" && computerSelection == "scissor")  {
+  playerScore += 1
+    result = ('You win!' + "<br>Player Score:" + playerScore + "<br>Computer Score:" + computerScore)
+    document.getElementById('result').innerHTML = result
+    return
+  
+  }
+    
+
 
   //PLAYER PAPER
   else if (playerSelection == "paper" && computerSelection == "paper")
-  console.log("You tied!");
+  console.log ("You tied!");
   else if (playerSelection == "paper" && computerSelection == "rock")
-  console.log("You won. Paper beats rock!");
+  console.log ("You won. Paper beats rock!");
   else if (playerSelection == "paper" && computerSelection == "scissor")
-  console.log("You lost. Scissor beats Paper!");
+  console.log ("You lost. Scissor beats Paper!");
     
   //PLAYER SCISSOR
   else if (playerSelection == "scissor" && computerSelection == "paper")
@@ -139,6 +169,8 @@ function playRound3() {
   else if (playerSelection == "scissor" && computerSelection == "scissor")
   console.log("You tied!");
 }
+
+
 
 function game() {
   return playRound3();
